b27d211c5af7d76280cdab222bee74c128b704e6
mrpnkt <mr@mrpnkt.com>
2015-12-16 13:15:46 +0100

    Create childtheme-functions.php

---
+++ childtheme-functions.php
@@ -0,0 +1,5 @@
+// Faster than @import
+add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
+function my_child_theme_scripts() {
+    wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
+}
