3dcbe7918fb8bcd0e7faacce56fd0bf02982cbe3
mrpnkt <mr@mrpnkt.com>
2015-12-16 13:16:35 +0100

    Update childtheme-functions.php

--- childtheme-functions.php
+++ childtheme-functions.php
@@ -1,5 +1,9 @@
+<?php
+
 // Faster than @import
 add_action( 'wp_enqueue_scripts', 'my_child_theme_scripts' );
 function my_child_theme_scripts() {
     wp_enqueue_style( 'parent-theme-css', get_template_directory_uri() . '/style.css' );
 }
+
+?>
