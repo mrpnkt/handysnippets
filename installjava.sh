069514e59e122fc935894b68312483202f33726d
mrpnkt <mr@mrpnkt.com>
2016-04-13 17:53:19 +0200

    Create installjava.sh

---
+++ installjava.sh
@@ -0,0 +1,8 @@
+su root
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" > /etc/apt/sources.list.d/webupd8team-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu trusty main" >> /etc/apt/sources.list.d/webupd8team-java.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+apt-get update
+apt-get install oracle-java8-installer
+java -version
+exit
